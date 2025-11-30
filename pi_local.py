9cada326eb1a8ba5a65c8a8f9df6e8e6b54f95ef
Matthew Thirkettle <matthew.thirkettle2003@gmail.com>
2025-11-30 20:50:41 +0000

    initial stuff

--- pi_local.py
+++ pi_local.py
@@ -1,12 +1,17 @@
 import time
 import cv2
 import socket
+import struct
 
 host_ip = 'IP_ADDRESS' #Ip address of remote server
 host_socket = 0000 #Socket of server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host_ip, host_socket))
+
+face_cas = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
 cam = cv2.VideoCapture(0)
 
@@ -16,4 +21,3 @@ if not cam.isOpened():
 
 print("Camera is active!")
 
-#Hellodwdwdwdwd
