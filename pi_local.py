8032504b7876132934eafcfbaf19e653a8487822
Matthew Thirkettle <matthew.thirkettle2003@gmail.com>
2025-12-01 00:49:47 +0000

    server_script

--- pi_local.py
+++ pi_local.py
@@ -4,7 +4,7 @@ import socket
 import struct
 
 host_ip = 'IP_ADDRESS' #Ip address of remote server
-host_socket = 0000 #Socket of server
+host_socket = 5000 #Socket of server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host_ip, host_socket))
